60ead989fa46325a3f3a532077d527921ab0c133
Artur Wyszyński <artur.wyszynski@hitomi.pl>
2017-11-17 12:41:13 +0100

    Fix Blinker/Clock

--- libspaghetti/nodes/blinker.cc
+++ libspaghetti/nodes/blinker.cc
@@ -20,43 +20,19 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "nodes/clock.h"
-#include "elements/logic/clock.h"
+#include "nodes/blinker.h"
+#include "elements/logic/blinker.h"
 
-#include <QSpinBox>
 #include <QTableWidget>
-
-using elements::logic::Clock;
 
 namespace nodes {
 
-void Clock::showProperties()
+void Blinker::showProperties()
 {
   showCommonProperties();
   showOutputsProperties();
 
-  propertiesInsertTitle("Clock");
-
-  int currentIndex = m_properties->rowCount();
-  m_properties->insertRow(currentIndex);
-
-  QTableWidgetItem *item{};
-  item = new QTableWidgetItem{ "Rate" };
-  item->setFlags(item->flags() & ~Qt::ItemIsEditable);
-  m_properties->setItem(currentIndex, 0, item);
-
-  elements::logic::Clock *const clock{ static_cast<elements::logic::Clock *const>(m_element) };
-
-  QSpinBox *rateValue = new QSpinBox{};
-  rateValue->setRange(10, 10000);
-  rateValue->setValue(static_cast<int>(clock->duration().count()));
-  rateValue->setSuffix("ms");
-  m_properties->setCellWidget(currentIndex, 1, rateValue);
-
-  QObject::connect(rateValue, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), [this](int a_value) {
-    elements::logic::Clock *const clockElement{ static_cast<elements::logic::Clock *const>(m_element) };
-    clockElement->setDuration(std::chrono::milliseconds(a_value));
-  });
+  propertiesInsertTitle("Blinker");
 }
 
 } // namespace nodes
