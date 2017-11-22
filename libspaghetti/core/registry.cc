410ca91a0f086bce0159977d356ab3f0b942ef52
Artur Wyszyński <artur.wyszynski@hitomi.pl>
2017-11-22 16:36:05 +0100

    Add Int2Float

--- libspaghetti/core/registry.cc
+++ libspaghetti/core/registry.cc
@@ -91,6 +91,7 @@ void Registry::registerInternalElements()
   registerElement<ui::IntInfo, nodes::ui::IntInfo>("Info (Int)", ":/values/const_int.png");
   registerElement<ui::PushButton, nodes::ui::PushButton>("Push Button (Bool)", ":/ui/push_button.png");
   registerElement<ui::ToggleButton, nodes::ui::ToggleButton>("Toggle Button (Bool)", ":/ui/toggle_button.png");
+  registerElement<values::Int2Float>("Int2Float", ":/unknown.png");
   registerElement<values::Degree2Radian>("Degree2Radian", ":/unknown.png");
   registerElement<values::Radian2Degree>("Radian2Degree", ":/unknown.png");
 
--- libspaghetti/elements/values/int_to_float.cc
+++ libspaghetti/elements/values/int_to_float.cc
@@ -20,20 +20,29 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#pragma once
-#ifndef ELEMENTS_VALUES_ALL_H
-#define ELEMENTS_VALUES_ALL_H
-
-#include "elements/values/const_bool.h"
-#include "elements/values/const_float.h"
-#include "elements/values/const_int.h"
-#include "elements/values/degree_to_radian.h"
 #include "elements/values/int_to_float.h"
-#include "elements/values/max_float.h"
-#include "elements/values/max_int.h"
-#include "elements/values/min_float.h"
-#include "elements/values/min_int.h"
-#include "elements/values/radian_to_degree.h"
-#include "elements/values/random_bool.h"
+#include "elements/package.h"
 
-#endif // ELEMENTS_VALUES_ALL_H
+namespace elements::values {
+
+Int2Float::Int2Float()
+{
+  setMinInputs(1);
+  setMaxInputs(1);
+  setMinOutputs(1);
+  setMaxOutputs(1);
+  addInput(ValueType::eInt, "Int");
+  addOutput(ValueType::eFloat, "Float");
+}
+
+bool Int2Float::calculate()
+{
+  if (!allInputsConnected()) return false;
+
+  int32_t const input{ std::get<int32_t>(*m_inputs[0].value) };
+  m_outputs[0].value = static_cast<float>(input);
+
+  return true;
+}
+
+} // namespace elements::values
